351c1260aa2c8d3539dbb7928120c9611aad013b
Antonio Cecere <antocecere77@gmail.com>
2019-10-20 15:22:36 +0200

    Automation dashboard

---
+++ src/test/java/com/automation/dashboard/SpringContextTest.java
@@ -0,0 +1,16 @@
+package com.automation.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.automation.dashboard.SpringBootAdminServerApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootAdminServerApplication.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
